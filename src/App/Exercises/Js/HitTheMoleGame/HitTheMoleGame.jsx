730345ace9c82ccccc81f6b8a5e9b33c4d0a83db
AdrianBakalarz <adrianbaakalarz@gmail.com>
2023-07-06 21:37:27 +0200

    dodanie funkcji wyświetlającej ranodomowe kreciki

--- src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
@@ -1,119 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './style.css';
-import Mole from './Mole.png';
-import { useEffect } from 'react';
-
-export const MoleGameSettings = () => {
-  const defaultGameTime = 2 * 60 * 1000;
-  const [gameTime, setGameTime] = useState(defaultGameTime); // 2000 * 60
-  const [moleCount, setMoleCount] = useState(1);
-  const [seconds, setSeconds] = useState(gameTime / 1000);
-  const [isCountingDown, setIsCountingDown] = useState(false);
-  const gameTimeOption = [
-    { label: '1 minuta', timeValue: 1 * 60 * 1000 },
-    { label: '2 minuty', timeValue: 2 * 60 * 1000 },
-    { label: '3 minuty', timeValue: 3 * 60 * 1000 },
-  ];
-  const moleCountOption = [
-    { label: '1 kret' },
-    { label: '2 krety' },
-    { label: '3 krety' },
-  ];
-  const startTimer = () => {
-    setSeconds(seconds - 1);
-    setIsCountingDown((current) => !current);
-  };
-  useEffect(() => {
-    setSeconds(defaultGameTime / 1000);
-  }, [gameTime]);
-
-  useEffect(() => {
-    let intervalId;
-    if (setIsCountingDown) {
-      intervalId = setInterval(() => {
-        setSeconds(seconds - 1);
-      }, 1000);
-    }
-    console.log(seconds.toString().padStart(2, 0));
-    return () => clearInterval(intervalId);
-  }, [seconds]);
-
-  return (
-    <>
-      <div className="moleGameOptions">
-        <p>
-          Gra polegająca na podążaniu za krecikiem i trafieniu na kwadrat, w
-          którym się pojawił.
-        </p>
-
-        <div className="gameOptionsButtons">
-          <div className="gameButtonsRows">
-            <div>
-              <h4>CZAS GRY</h4>
-              {gameTimeOption.map(({ label, timeValue }) => (
-                <button
-                  className={gameTime === timeValue ? 'activeButton' : ''}
-                  onClick={() => setGameTime(timeValue)}
-                >
-                  {label}
-                </button>
-              ))}
-            </div>
-            <div>
-              <h4>LICZBA KRETÓW</h4>
-              {moleCountOption.map(({ label }) => (
-                <button
-                  className={
-                    moleCount === Number(label[0]) ? 'activeButton' : ''
-                  }
-                  onClick={() => setMoleCount(Number(label[0]))}
-                >
-                  {label}
-                </button>
-              ))}
-            </div>
-            <div>
-              <h4>PRZYCISKI STERUJĄCE</h4>
-              <button>START</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-export const MoleGameBoard = (props) => {
-  return (
-    <div className="moleGame">
-      {props.moleArray.map((mole, index) => {
-        <div>
-          <span>
-            {mole.isVisible ? (
-              <img src={Mole} onClick={() => props.hitTheMole(index)} />
-            ) : null}
-          </span>
-        </div>;
-      })}
-    </div>
-  );
-};
+import MoleGameSettings from './MoleGameSettings';
+import MoleGameBoard from './MoleGameBoard';
 
 export function HitTheMoleGame() {
+  const defaultGameTime = 2 * 60 * 1000;
+  const [gameTime, setGameTime] = useState(defaultGameTime / 1000);
+  // const [seconds, setSeconds] = useState(gameTime / 1000);
+  const [moleCount, setMoleCount] = useState(1);
+  const [scoreCount, setScoreCount] = useState(0);
   const [moleArray, setMoleArray] = useState(
-    Array(10).fill({ isVisible: false, isWhacked: false })
+    Array(10).fill({ isVisible: true, isWhacked: false })
   );
+
+  const CountdownTimer = ({ initialCountdownState }) => {
+    const [countdown, setCountdown] = useState(initialCountdownState);
+    useEffect(() => {
+      if (!countdown) return;
+      const interval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1000);
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }, [countdown]);
+    return (
+      <div>
+        <p>time remaining: {countdown / 1000} seconds</p>
+      </div>
+    );
+  };
 
   function hitTheMole(index) {
     console.log(moleArray[index].isWhacked);
-
     if (!moleArray[index].isVisible) return;
     moleArray[index].isWhacked = !moleArray[index].isWhacked;
     console.log(moleArray[index].isWhacked);
   }
+
+  function showRandomMole() {
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    const random = getRandomIntInclusive(0, moleArray.length - 1);
+    console.log(random);
+
+    setMoleArray((previousMoleArray) =>
+      previousMoleArray.map((mole, index) => {
+        const updatedMole = { ...mole };
+        updatedMole.isVisible = index === random;
+        console.log(updatedMole);
+        return updatedMole;
+      })
+    );
+    console.log(moleArray);
+  }
+
   return (
     <>
-      <MoleGameSettings />
-      <MoleGameBoard moleArray={moleArray} hitTheMole={hitTheMole} />
+      <MoleGameSettings
+        gameTime={gameTime}
+        moleCount={moleCount}
+        setGameTime={setGameTime}
+        setMoleCount={setMoleCount}
+      />
+      <MoleGameBoard
+        scoreCount={scoreCount}
+        moleArray={moleArray}
+        hitTheMole={hitTheMole}
+      />
+      <CountdownTimer initialCountdownState={gameTime} />
+      <button onClick={() => showRandomMole()}>KRECIK</button>
     </>
   );
 }
