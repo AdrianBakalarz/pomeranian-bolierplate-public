dff09672684ca850ace2c1ac9af3bbd0dab59e8d
AdrianBakalarz <adrianbaakalarz@gmail.com>
2023-07-12 19:56:12 +0200

    zmiana nazw plików + usprawnienie gry ze stylowaniem

---
+++ src/App/Exercises/Js/MemoGame/MemoBoard.jsx
@@ -0,0 +1,96 @@
+import { useState, useEffect } from 'react';
+
+import formatTime from './FormatTime';
+
+export const MemoBoard = ({
+  gameTime,
+  setGameStart,
+  boardSize,
+  score,
+  setScore,
+  setFinalSettings,
+}) => {
+  const [memoArray, setMemoArray] = useState([]);
+  useEffect(() => {
+    const generateMemoArray = () => {
+      const memoSymbols = 'ABCDEFGHIJKLMNOPRSTUWXYZ'; // Symbole 24, max tablica 48
+      const memoCardsSymbols = [];
+
+      for (let i = 0; i < boardSize / 2; i++) {
+        const index = Math.floor(Math.random() * (24 - 0) + 0);
+        const letter = memoSymbols.charAt(index);
+
+        memoCardsSymbols.push({
+          symbol: letter,
+          isVisibleLetteer: false,
+          isVisibleBox: true,
+        });
+      }
+
+      console.log(memoCardsSymbols);
+      const shuffledCards = [...memoCardsSymbols, ...memoCardsSymbols].sort(
+        () => Math.random() - 0.5
+      );
+      console.log(shuffledCards);
+      setMemoArray(shuffledCards);
+    };
+
+    generateMemoArray();
+  }, [boardSize]);
+
+  const VisableFunction = (index, memo) => {
+    const updatedMemoArray = [...memoArray];
+    updatedMemoArray[index] = { ...memo, isVisible: !memo.isVisibleLetteer };
+    const isVisibleLetters = updatedMemoArray.filter((memo) => memo.isVisible);
+    console.log({ index });
+
+    if (isVisibleLetters.length === 2) {
+      setTimeout(() => {
+        updatedMemoArray.forEach((memo) => {
+          memo.isVisible = false;
+        }, 2000);
+      });
+    }
+
+    return updatedMemoArray;
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h2 className="item">CZAS GRY: </h2>
+        <h2 className="timeAndScore">{formatTime(gameTime)}</h2>
+        <h2 className="item">LICZBA RUCHÓW:</h2>
+        <h2 className="timeAndScore">{score}</h2>
+        <h2 className="item">PRZYCISKI STERUJĄCE</h2>
+        <div className="stopButton">
+          <button
+            onClick={() => {
+              setGameStart(false);
+              setFinalSettings(boardSize);
+            }}
+          >
+            STOP
+          </button>
+        </div>
+      </div>
+
+      <div className="memoGame">
+        {memoArray.map((memo, index) => {
+          return (
+            <div key={index}>
+              <span
+                onClick={() => {
+                  setScore(++score);
+                  setMemoArray(VisableFunction(index, memo));
+                }}
+              >
+                <div id="square">{memo.isVisible && memo.symbol}</div>
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
--- src/App/Exercises/Js/MemoGame/MemoGame.jsx
+++ src/App/Exercises/Js/MemoGame/MemoGame.jsx
@@ -1,48 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import Settings from './Settings';
-import Board from './Board';
-
-export const MemoGame = () => {
-  const defaultBoardSize = '';
-  const [gameTime, setGameTime] = useState(0);
+import './style.css';
+import { MemoSettings } from './MemoSettings';
+import { MemoBoard } from './MemoBoard';
+export function MemoGame() {
+  const defaultBoardSize = 16;
   const [boardSize, setBoardSize] = useState(defaultBoardSize);
-  const [clicks, setClicks] = useState(0);
-  const [gameStarted, setGameStarted] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const [memoArray, setMemoArray] = useState(Array(16).fill('A'));
+  const [gameStart, setGameStart] = useState(false);
+  const [gameTime, setGameTime] = useState(0);
+  const [score, setScore] = useState(0);
+  const [finalSettings, setFinalSettings] = useState('');
 
   useEffect(() => {
-    let countdownInterval;
-
-    if (!countdownInterval && gameStarted) {
-      countdownInterval = setInterval(() => {
-        setCounter((previousCounter) => previousCounter + 1);
+    let intervalId;
+    if (gameStart) {
+      intervalId = setInterval(() => {
+        setGameTime((pervGameTime) => pervGameTime + 1);
       }, 1000);
-
-      return () => clearInterval(countdownInterval);
     }
-  }, [counter, gameStarted]);
 
-  useEffect(() => {
-    if (!gameStarted || gameStarted) setCounter(0);
-  }, [gameStarted, gameTime]);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [gameStart, gameTime]);
 
-  function handleCellClick() {
-    setClicks((prevValue) => prevValue + 1);
-  }
   return (
     <>
-      <Settings
-        boardSize={boardSize}
-        setBoardSize={setBoardSize}
-        startStopGame={() => setGameStarted((prev) => !prev)}
-        gameStarted={gameStarted}
-      />
-      <Timer time={counter} />
+      <p>{gameTime}</p>
+      {!gameStart && (
+        <MemoSettings
+          boardSize={boardSize}
+          setBoardSize={setBoardSize}
+          setGameStart={setGameStart}
+          score={score}
+          gameTime={gameTime}
+          finalSettings={finalSettings}
+          setScore={setScore}
+          setGameTime={setGameTime}
+        />
+      )}
+      {gameStart && (
+        <MemoBoard
+          gameTime={gameTime}
+          setGameStart={setGameStart}
+          boardSize={boardSize}
+          score={score}
+          setScore={setScore}
+          setFinalSettings={setFinalSettings}
+        />
+      )}
     </>
   );
-};
-
-const Timer = ({ time }) => {
-  <h4>CZAS GRY {time}</h4>;
-};
+}
