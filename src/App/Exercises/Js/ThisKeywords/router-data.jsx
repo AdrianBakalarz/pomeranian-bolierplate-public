1db7652999f20bc130312df926983e838049ca11
AdrianBakalarz <adrianbaakalarz@gmail.com>
2023-07-20 21:01:28 +0200

    examples of using -this with bind, apply, call

---
+++ src/App/Exercises/Js/ThisKeywords/router-data.jsx
@@ -0,0 +1,10 @@
+import { JsThisKeywordsExercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'js-this',
+  date: '20.07.2023',
+  linkLabel: 'This w JavaScript',
+  blockNo: 33,
+  element: <JsThisKeywordsExercise />,
+  tags: ['this', 'constructor', 'oop', 'object', 'javascript'],
+};
