36eba9187aafb7d952e813b789251316a481e9ce
AdrianBakalarz <adrianbaakalarz@gmail.com>
2023-08-01 17:29:43 +0200

    exercise with using different server

---
+++ src/App/Exercises/ReactExercises/LocalDevAndFetch/router-data.jsx
@@ -0,0 +1,10 @@
+import { Swagger } from './index';
+
+export const blockRouterMetaData = {
+  path: 'local-dev=and-fetch',
+  date: '31.07.2023',
+  linkLabel: 'local dev z fetchem',
+  blockNo: 36,
+  element: <Swagger />,
+  tags: ['endpoint', 'fetch', 'rest', 'api', 'javascript'],
+};
