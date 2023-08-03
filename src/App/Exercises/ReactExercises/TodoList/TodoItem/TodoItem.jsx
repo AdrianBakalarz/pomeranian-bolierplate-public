f5a891a83f51f89cd4070f48ca091f1b51b5046b
AdrianBakalarz <adrianbaakalarz@gmail.com>
2023-08-03 18:07:56 +0200

    adding feature to delete todo's

---
+++ src/App/Exercises/ReactExercises/TodoList/TodoItem/TodoItem.jsx
@@ -0,0 +1,77 @@
+import './TodoItem.css';
+import { formatDate } from '../../../../helpers/formatDate';
+import { BinIcon } from '../../../../Images/Icons/BinIcon/BinIcon';
+import { BASE_API_URL } from '..';
+import axios from 'axios';
+import { useState } from 'react';
+
+export function TodoItem({ todo, handleFetchTodoData }) {
+  const { id, title, author, createdAt, isDone, doneDate, note } = todo;
+  const [isRemoveError, setRemoveError] = useState(false);
+
+  const itemClasses = `todo-item ${isDone ? 'todo-item--darker' : ''}`;
+
+  function handleRemoveClick() {
+    axios
+      .delete(BASE_API_URL + /todo/ + id)
+      .then((response) => {
+        console.log('usunięty todo id: ', response);
+        handleFetchTodoData();
+      })
+      .catch((error) => {
+        console.log('usuwanie nie udało się: ', error);
+        setRemoveError(true);
+      });
+
+    axios.delete(BASE_API_URL + '/todo/' + id);
+  }
+
+  return (
+    <div className={itemClasses}>
+      <div className="todo-item__wrapper">
+        <h3 className="todo-item__wrapper__title">{title}</h3>
+        <div className="todo-item__wrapper__text todo-item__wrapper__text--smaller">
+          {author}
+        </div>
+        <div className="todo-item__wrapper__text todo-item__wrapper__text--smaller">
+          {formatDate(createdAt)}
+        </div>
+        <p className="todo-item__wrapper__text">{note}</p>
+      </div>
+      <div className="todo-item__actions">
+        <button
+          className="todo-item__actions__button 
+          todo-item__actions__icon"
+          onClick={() => handleRemoveClick()}
+        >
+          <BinIcon isError={isRemoveError} />
+        </button>
+        {isRemoveError && (
+          <div className="todo-item__actions__error-message">
+            nie udało się usunąć
+          </div>
+        )}
+
+        {isDone && (
+          <>
+            <div
+              className="todo-item__actions__icon 
+              todo-item__actions__icon--check-mark"
+            >
+              &#10003;
+            </div>
+            <div>{formatDate(doneDate)}</div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+/*
+    utworzenie buttona
+    wybór elementu do usunięcia
+    request do API
+    sprawdzenie czy API wykonało usunięcie todo'sa
+    zaktualizowanie listy todo'sów po usunięciu
+*/
--- src/App/Exercises/ReactExercises/TodoList/index.jsx
+++ src/App/Exercises/ReactExercises/TodoList/index.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './style.css';
-import { formatDate } from '../../../helpers/formatDate';
+import { TodoItem } from './TodoItem/TodoItem';
 
-const BASE_API_URL = 'http://localhost:3333/api';
+export const BASE_API_URL = 'http://localhost:3333/api';
 
 export function TodoList() {
-  const [getTodoList, setTodoList] = useState([]);
-  const [getError, setError] = useState([]);
+  const [todoList, setTodoList] = useState([]);
+  const [error, setError] = useState([]);
 
   const handleFetchTodoData = async () => {
     const timeOutDuration = 5000; //5sec czekania na odpoiedź serwera
@@ -39,44 +39,19 @@ export function TodoList() {
 
   return (
     <div className="todo-container">
-      <h2 className="todo-container__title">Todo List 2</h2>
+      <h2 className="todo-container__title">Todo List</h2>
 
-      {getError && <p>{getError}</p>}
+      {error && <p>{error}</p>}
 
       <div className="todo-container__list">
-        {getTodoList.length > 0 &&
-          getTodoList.map((todo) => {
-            const itemClasses = `todo-container__list__item ${
-              todo.isDone ? 'todo-container__list__item--darker' : ''
-            }`;
-
+        {todoList.length > 0 &&
+          todoList.map((todo) => {
             return (
-              <div className={itemClasses} key={todo.id}>
-                <div className="todo-container__list__item__wrapper">
-                  <h3 className="todo-container__list__item__wrapper__title">
-                    {todo.title}
-                  </h3>
-                  <div className="todo-container__list__item__wrapper__text todo-container__list__item__wrapper__text--smaller">
-                    {todo.author}
-                  </div>
-                  <div className="todo-container__list__item__wrapper__text todo-container__list__item__wrapper__text--smaller">
-                    {formatDate(todo.createdAt)}
-                  </div>
-                  <p className="todo-container__list__item__wrapper__text">
-                    {todo.note}
-                  </p>
-                </div>
-                <div className="todo-container__list__item__side">
-                  {todo.isDone && (
-                    <>
-                      <div className="todo-container__list__item__side__checkmark">
-                        &#10003;
-                      </div>
-                      <div>{formatDate(todo.doneDate)}</div>
-                    </>
-                  )}
-                </div>
-              </div>
+              <TodoItem
+                todo={todo}
+                key={todo.id}
+                handleFetchTodoData={handleFetchTodoData}
+              />
             );
           })}
       </div>
